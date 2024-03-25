997dd78bc05560136fbec4733a4729e944b08324
Emergency1999 <johannes@schmid-company.de>
2024-03-25 12:07:21 +0100

    v2.0: moved dashboard and config files to external folder

--- service.sh
+++ service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SERVICE_NAME="traefik"
-SERVICE_VERSION="v1.1"
+SERVICE_VERSION="v2.0"
 
 set -e
 
@@ -15,6 +15,15 @@ source ./borg/borg.sh
 # ATTACHMENTS
 att_setup() {
   docker network create traefik &>/dev/null || true
+  
+  # if CONFIG_PATH does not exists, copy default
+  if [[ ! -d "$SERVICES_DIR/$CONFIG_PATH" ]]; then
+    echo "Copying default configuration to $CONFIG_PATH"
+    mkdir -p $SERVICES_DIR/$CONFIG_PATH
+    cp $SERVICE_DIR/dashboard.example.yml $SERVICES_DIR/$CONFIG_PATH/dashboard.yml
+    echo "Manually add DASHBOARD_DOMAIN and DASHBOARD_BASICAUTH_USER to $CONFIG_PATH/dashboard.yml"
+    exit 1
+  fi
 }
 
 # MAIN
